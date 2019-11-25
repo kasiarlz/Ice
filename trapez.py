34c1bf2ce6a235c828329f00e3345631181969e8
kasiarlz <58173725+kasiarlz@users.noreply.github.com>
2019-11-25 13:19:48 +0100

    Add files via upload

---
+++ trapez.py
@@ -0,0 +1,9 @@
+print('Obliczanie pola trapezu\n')
+print('Podaj bok A:' ,end=' ')
+bokA = float(input())
+print('podaj bok B',end= ' ')
+bokB = float(input()) 
+print('Podaj wysokość:' , end=' ')
+wys= float(input())
+pole=(bokA+bokB)*wys/2
+print('Pole wynosi: \t%.2f'%pole)
